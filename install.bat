2fe699753ec6e878079b8a7fd9ee219ba97c344b
Super-Fractal <crypto.sup4@gmail.com>
2025-05-10 02:07:03 +0900

    Installation without git

--- install.bat
+++ install.bat
@@ -3,12 +3,15 @@
 mkdir venu
 cd venu
 python -m venv env
-
 call env/Scripts\activate
 python -m ensurepip --upgrade
-git clone https://github.com/Super-Fractal/LSVM
-cd LSVM
 
+curl -L -o LSVM.zip https://github.com/Super-Fractal/LSVM/archive/refs/heads/main.zip
+powershell -Command "Expand-Archive -Path LSVM.zip -DestinationPath LSVM"
+robocopy LSVM\LSVM-main LSVM /E /MOVE
+rmdir /s /q LSVM\LSVM-main
+
+cd LSVM
 pip install -r requirements.txt
 python -c "print('Install Completed')"
 pause
