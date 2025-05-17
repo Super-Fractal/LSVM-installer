98b99d726d699fc30330d0bc3a764018d908b129
Super-Fractal <crypto.sup4@gmail.com>
2025-05-17 18:06:20 +0900

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ cd venu
 python -m venv env
 call env/Scripts\activate
 python -m ensurepip --upgrade
-curl -L -o LSVM.zip https://github.com/Super-Fractal/LSVM/releases/download/v0.2/LSVM.zip
+curl -L -o LSVM.zip https://github.com/Super-Fractal/LSVM/releases/download/v0.3/LSVM.zip
 powershell -Command "Expand-Archive -Path LSVM.zip -DestinationPath LSVM"
 robocopy LSVM\LSVM-main LSVM /E /MOVE
 rmdir /s /q LSVM\LSVM-main
